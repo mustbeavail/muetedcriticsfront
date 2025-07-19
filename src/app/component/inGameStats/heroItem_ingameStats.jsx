f00cd242777ee69246eabaef3e5e34240b1e27e3
oneidsin <oneidsin@naver.com>
2025-07-19 14:20:38 +0900

    영웅이 보유한 아이템 수 완료

--- src/app/component/inGameStats/heroItem_ingameStats.jsx
+++ src/app/component/inGameStats/heroItem_ingameStats.jsx
@@ -1,45 +1,65 @@
 "use client"
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useMemo, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-export default function HeroItem_IngameStats({ inGameStatsData }) {
-    const chartData = inGameStatsData.map(entry => {
-        const heroName = entry.heros.heroes_name;
-        const items = entry.heros.item_list;
+const URL = process.env.NEXT_PUBLIC_API_URL;
 
-        const itemCount = items ? Object.keys(items).length : 0;
-
-        return {
-            heroes_name: heroName,
-            item_count: itemCount,
-        };
-    });
-
-    // 차트 페이징 처리
-    const itemsPerPage = 10;
+export default function HeroItem_IngameStats() {
     const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [sortOrder, setSortOrder] = useState('desc');
+    const [heroItemData, setHeroItemData] = useState([]);
 
-    const totalPages = Math.ceil(chartData.length / itemsPerPage);
+    const itemsPerPage = 10;
 
-    const pagedData = chartData.slice(
-        (currentPage - 1) * itemsPerPage,
-        currentPage * itemsPerPage
-    );
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+        if (!token) return;
+        getHeroItemData(token);
+    }, [sortOrder]);
+
+    // 영웅이 보유한 아이템 수 불러오기
+    const getHeroItemData = async (token) => {
+        try {
+            const { data } = await axios.get(`${URL}/get/hero-item-count`, {
+                headers: { Authorization: token },
+                params: {
+                    sortOrder: sortOrder
+                }
+            });
+            //console.log(data);
+            setHeroItemData(data.list);
+            setTotalPages(Math.ceil(data.list.length / itemsPerPage));
+        } catch (error) {
+            console.log('영웅별 보유 아이템 수 조회 실패: ', error);
+        }
+    }
+
+
+    // 페이지네이션 데이터
+    const pagedData = useMemo(() => {
+        const start = (currentPage - 1) * itemsPerPage;
+        return heroItemData.slice(start, start + itemsPerPage);
+    }, [heroItemData, currentPage]);
 
     return (
         <div className={"ingameStats-chartWrapper"}>
             <div className="userStats-filterBox-wrapper">
                 <h2 className={"userStats-title"}>영웅별 보유 아이템 개수</h2>
                 <div className="itemStats-filterBox">
-                    <select className="itemStats-select">
-                        <option value="전체">시즌 선택</option>
-                    </select>
-                    <select className="itemStats-select">
-                        <option value="전체">높은 순</option>
+                    <select className="itemStats-select"
+                        value={sortOrder}
+                        onChange={(e) => {
+                            setSortOrder(e.target.value);
+                            setCurrentPage(1);
+                        }}>
+                        <option value="desc">높은 순</option>
+                        <option value="asc">낮은 순</option>
                     </select>
                 </div>
             </div>
-            <ResponsiveContainer width="100%" height={1120}>
+            <ResponsiveContainer width="100%" height={1000}>
                 <BarChart
                     data={pagedData}
                     layout="vertical"
@@ -54,26 +74,31 @@ export default function HeroItem_IngameStats({ inGameStatsData }) {
 
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis type="number" />
-                    <YAxis dataKey="heroes_name" type="category" />
+                    <YAxis dataKey="heroName" type="category" />
                     <Tooltip
                         formatter={(value) => `${value} 개`}
                         contentStyle={{ fontSize: 15, background: '#1c1b23', color: '#fff' }}
                         cursor={{ fill: '#1c1b23' }}
                     />
-                    <Bar dataKey="item_count" fill="url(#itemCountGradient)" />
+                    <Bar dataKey="itemCount" fill="url(#itemCountGradient)" />
                 </BarChart>
             </ResponsiveContainer>
-            {/* 페이징 버튼 */}
+            {/* 페이지네이션 */}
             <div className="pagination">
-                {Array.from({ length: totalPages }, (_, i) => (
-                    <button
-                        key={i}
-                        onClick={() => setCurrentPage(i + 1)}
-                        className={currentPage === i + 1 ? "active" : ""}
-                    >
-                        {i + 1}
-                    </button>
-                ))}
+                {(() => {
+                    const buttons = [];
+                    for (let i = 0; i < totalPages; i++) {
+                        buttons.push(
+                            <button
+                                key={i}
+                                onClick={() => setCurrentPage(i + 1)}
+                                className={currentPage === i + 1 ? "active" : ""}>
+                                {i + 1}
+                            </button>
+                        )
+                    }
+                    return buttons;
+                })()}
             </div>
         </div>
     );
